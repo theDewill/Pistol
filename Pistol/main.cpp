af0ecb463757962e1b654885c2bf5eaf96730e7c
Nomin Sendinu <soloriderns@gmail.com>
2023-07-02 23:50:40 +0530

    Initial Commit

---
+++ Pistol/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pistol
+//
+//  Created by Nomin Sendinu on 2023-07-02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
